4c946ebd56bb8f1092d667938724600df29d2ac2
de-mat_b <de-mat_b@epitech.eu>
2017-05-23 21:21:54 +0200

    MASTER Added preloading page animation

--- yoru.js
+++ yoru.js
@@ -9,6 +9,9 @@ import YoruObject from './yoru-object';
 import { Logger, Scribe, Run } from './komono/utils';
 import { ShadowMaker, TemplateConsumer } from './kage/shadow';
 
+const PRELOAD_CSS =
+  'body{transform:scale(1);transform-origin:top;opacity:1;transition:transform .6s,opacity .6s;}body.yoru-loading{transform:scale(1.05);opacity:0;transition:none;}';
+
 class Yoru extends YoruObject {
   static Logger = Logger;
   static Scribe = Scribe;
@@ -18,13 +21,33 @@ class Yoru extends YoruObject {
     super(...arguments);
     this.templateConsumer = new TemplateConsumer(this);
     this.shadowMaker = new ShadowMaker(this.templateConsumer);
+
+    this._initPreload();
+  }
+
+  _initPreload() {
+    this.preloadStyle = document.createElement('style');
+    this.preloadStyle.innerHTML = PRELOAD_CSS;
+    document.head.appendChild(this.preloadStyle);
+  }
+
+  _destroyPreload() {
+    Run.async(() => {
+      document.body.classList.remove('yoru-loading');
+      Run.later(() => {
+        this.preloadStyle.parentNode.removeChild(this.preloadStyle);
+        this.preloadStyle = null;
+      }, 650);
+    });
   }
 
   boot() {
     Logger.info('Booting Yoru');
 
+    document.body.classList.add('yoru-loading');
     this.templateConsumer.consume();
     this.shadowMaker.init();
+    this._destroyPreload();
   }
 
   registerComponent(name, opts = {}) {
