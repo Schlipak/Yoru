6af571ca8a744e444dd18aac9e19ccb850b0260e
Guillaume de Matos guillaume.dematos@facilecomm.com <guillaume.dematos@facilecomm.com>
2017-06-21 15:09:12 +0200

    Tooling: Show help when no command given

--- bin/yoru.js
+++ bin/yoru.js
@@ -1,22 +1,26 @@
 #!/usr/bin/env node
 
-const commander = require('commander');
+const program = require('commander');
 const newApp = require('./scripts/new');
 const server = require('./scripts/server');
 
-commander.version('0.1.0');
-commander
+program.version('0.1.0');
+program
   .command('new <name>')
   .description('create a new Yoru project')
   .action((name, opts) => newApp(name, opts));
-commander
+program
   .command('serve')
   .alias('run')
   .option('-p, --port <n>', 'set the port number to listen to', parseInt)
   .description('runs the app')
-  .action((options) => {
+  .action(options => {
     const port = options.port || 3000;
     const serv = new server(port);
     serv.boot(port);
   });
-commander.parse(process.argv);
+program.parse(process.argv);
+
+if (program.args.length === 0) {
+  program.help();
+}
